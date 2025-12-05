05db9f46dd6d0f3c5dd2646701b4fc0fd0040bc7
amandubey-01 <aman351dubey@gmail.com>
2025-12-05 23:00:50 +0530

    Modified authConroller.js --> added login function, and Modified authRoutes.js --> added login routes

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -41,14 +41,63 @@ const signup = async (req, res) => {
         {expiresIn: process.env.JWT_EXPIRES_IN}
     );
 
-    user.password = undefined; // <------ we don't want to send passoword in token
+    // user.password = undefined; // <------ we don't want to send passoword in token
 
     res.status(200).json({
         status: "success",
         token,
-        data: {user}
+        data: {
+            user: {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                preferences: user.preferences
+            }
+        }
     });
 
 };
 
-module.exports = {signup};
+const login = async (req,res) => {
+    const {email, password} = req.body;
+
+    // 1. Check if email and password were passed.
+    if (!email || !password){
+        return res.status(400).json({
+            status: "fail",
+            message: "Please provide email and password"
+        });
+    }
+
+    // 2. Check if user exists.
+    const user = findUserByEmail(email);
+    if(!user){
+        return res.status(401).json({
+            status: "fail",
+            message: "Invalid email or password"
+        });
+    }
+
+    // 3. Check if password is correct.
+    const isPasswordCorrect = await bcrypt.compare(password, user.password)
+    if(!isPasswordCorrect){
+        return res.status(401).json({
+            status: "fail",
+            message: "Invalid email or password"
+        });
+    }
+
+    const token = jwt.sign(
+        {id:user.id},
+        process.env.JWT_SECRET,
+        {expiresIn: process.env.JWT_EXPIRES_IN}
+    );
+
+    res.status(200).json({
+        status: "success",
+        token
+    });
+
+}
+
+module.exports = {signup, login};
