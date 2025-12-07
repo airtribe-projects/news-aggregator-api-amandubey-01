1c8474d88b884e54867f1e823968c08e4f7b5a48
amandubey-01 <aman351dubey@gmail.com>
2025-12-07 11:44:58 +0530

    refactor getting news from api

--- src/controllers/newsController.js
+++ src/controllers/newsController.js
@@ -1,38 +1,9 @@
-const axios = require('axios');
+// src/controllers/newsController.js
+const { getPersonalizedNews } = require('../services/newsService');
 
 const getNews = async (req, res) => {
-    try{
-        const {preferences} = req.user;
-        
-        // If no preferences, return empty array
-        if(!preferences || preferences.length == 0){
-            return res.status(200).json({
-                news: []
-            });
-        }
-        // Join preferences with comma for GNews query
-        const query = preferences.join(',');
-
-        const response = await axios.get('https://api.gnews.io/v4/top-headlines',{
-            params:{
-                token: process.env.NEWS_API_KEY,
-                topic: query,
-                lang: 'en',
-                country: 'us'
-            }
-        });
-
-        const articles = response.data.articles || [];
-
-        res.status(200).json({
-            news: articles
-        });
-    }catch(error){
-        // Even if api fails, don't crash - return empty array
-        res.status(200).json({
-            news: []
-        });
-    }
+  const news = await getPersonalizedNews(req.user.preferences);
+  res.status(200).json({ news });
 };
 
-module.exports = {getNews};
+module.exports = { getNews };
